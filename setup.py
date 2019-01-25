fcc5273f4a1c4ecf66b75fec16c00e49d2e51ae9
Thomas Grønli <thomas.gronli@gmail.com>
2019-01-25 09:53:44 +0100

    Added linux build steps

--- setup.py
+++ setup.py
@@ -6,23 +6,41 @@ import tensorflow as tf
 import pathlib
 from setuptools import Extension
 from setuptools.command.build_ext import build_ext
+import platform
+
+system = platform.system()
 
 
-has_cuda = 'CUDA_PATH' in os.environ
+if system == 'Windows':
+      has_cuda = 'CUDA_PATH' in os.environ
+      include_dirs = [str(tf.sysconfig.get_include())]
+      library_dirs = [str(pathlib.Path(tf.sysconfig.get_lib()) / 'python')]
+      libraries = ['pywrap_tensorflow_internal']
 
-include_dirs = [str(tf.sysconfig.get_include())]
-library_dirs = [str(pathlib.Path(tf.sysconfig.get_lib()) / 'python')]
-libraries = ['pywrap_tensorflow_internal']
+      macros = [("NOMINMAX",None),("COMPILER_MSVC",None),("USE_MULTITHREADING",None)]
+      sources = ['tensorspline/src/splines.cc', 'tensorspline/src/splinegrid_cpu.cc']
 
-macros = [("NOMINMAX",None),("COMPILER_MSVC",None),("USE_GPU",None),("USE_MULTITHREADING",None)]
-sources = ['tensorspline/src/splines.cc', 'tensorspline/src/splinegrid_cpu.cc']
+      if has_cuda:
+            macros.append(("USE_GPU",None))
+            include_dirs.append(str(pathlib.Path(os.environ['CUDA_PATH']) / 'include'))
+            library_dirs.append(str(pathlib.Path(os.environ['CUDA_PATH']) / 'lib' / 'x64'))
+            libraries.extend(['cuda','cudart','nvrtc'])
+            sources.append('tensorspline/src/splinegrid_gpu.cc')
 
-if has_cuda:
-      include_dirs.append(str(pathlib.Path(os.environ['CUDA_PATH']) / 'include'))
-      library_dirs.append(str(pathlib.Path(os.environ['CUDA_PATH']) / 'lib' / 'x64'))
-      libraries.extend(['cuda','cudart','nvrtc'])
-      sources.append('tensorspline/src/splinegrid_gpu.cc')
+if system == 'Linux':
+      has_cuda = 'CUDA_ROOT' in os.environ
+      include_dirs = [str(tf.sysconfig.get_include())]
+      library_dirs = [str(pathlib.Path(tf.sysconfig.get_lib()))]
+      libraries = ['tensorflow_framework']
 
+      macros = [("_GLIBCXX_USE_CXX11_ABI", "0"),("USE_MULTITHREADING",None)]
+      sources = ['tensorspline/src/splines.cc', 'tensorspline/src/splinegrid_cpu.cc']
+      if has_cuda:
+            macros.append(("USE_GPU",None))
+            include_dirs.append(str(pathlib.Path(os.environ['CUDA_ROOT']) / 'include'))
+            library_dirs.append(str(pathlib.Path(os.environ['CUDA_ROOT']) / 'lib64'))
+            libraries.extend(['cuda','cudart','nvrtc'])
+            sources.append('tensorspline/src/splinegrid_gpu.cc')
 
 
 tensorspline = Extension('tensorspline.tensorspline_library',
@@ -30,12 +48,12 @@ tensorspline = Extension('tensorspline.tensorspline_library',
                     include_dirs = include_dirs,
                     libraries = libraries,
                     library_dirs = library_dirs,
-                    sources = sources
+                    sources = sources,
+                    extra_compile_args=['-std=c++11']
                     )
 
 class custom_build_ext(build_ext):
       def get_export_symbols(self,ext):
-            print(self.get_ext_filename(ext.name))
             return ext.export_symbols
 
 setuptools.setup(name='TensorSpline',
